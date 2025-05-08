cfed16ad8362ef593f8b5aade828eeee89863d62
RENTAL-01\student <fca2306010024@edu.fca.ac.jp>
2025-05-08 16:18:02 +0900

    インターン課題

---
+++ 根本悠希_インターン課題_20250507/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace 根本悠希_インターン課題_20250507
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(552, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "参照";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Reference_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(685, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "削除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.deletion_Click);
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(567, 314);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(112, 33);
+            this.PlayButton.TabIndex = 2;
+            this.PlayButton.Text = "再生";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 45);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(447, 170);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "ここに.wavまたはmp3ファイルをドラック＆ドロップしてください";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 239);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(534, 163);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ファイル名\t";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "\n拡張子";
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "日付";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "サイズ";
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(根本悠希_インターン課題_20250507.Form1);
+            this.form1BindingSource.CurrentChanged += new System.EventHandler(this.form1BindingSource_CurrentChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ 根本悠希_インターン課題_20250507/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NAudio.Wave;
+
+namespace 根本悠希_インターン課題_20250507
+{
+    public partial class Form1 : Form
+    {
+        const float Byte = 1024.0f;
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        private string currentFilePath = null; // 現在の再生ファイルパスを追跡
+
+        public Form1()
+        {
+            InitializeComponent();
+            //ドラック＆ドロップをテキストボックスで有効にする
+            textBox2.AllowDrop = true;
+            // ドラッグされたファイルを受け入れた際のイベントハンドラーを追加
+            textBox2.DragEnter += TextBox2_DragEnter;
+            textBox2.DragDrop += TextBox2_DragDrop;
+        }
+
+        private void Reference_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "音楽ファイルを選択してください";
+            openFileDialog1.Filter = "音楽ファイル (*.mp3;*.wav)|*.mp3;*.wav|すべてのファイル (*.*)|*.*";
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            openFileDialog1.Multiselect = true; // 複数選択を許可
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+
+                foreach (string filePath in openFileDialog1.FileNames)
+                {
+                    FileInfo fi = new FileInfo(filePath);
+                    // ファイルサイズをキロバイトに変換
+                    double fileSizeKB = fi.Length / Byte;
+
+                    ListViewItem item = new ListViewItem(fi.Name);
+                    item.SubItems.Add(fi.Extension);
+                    item.SubItems.Add(fi.CreationTime.ToString("yyyy-MM-dd HH:mm"));
+                    item.Tag = fi.FullName; // パスを保存
+                    double fileSizeMB = fileSizeKB / Byte;
+                    item.SubItems.Add(fileSizeMB.ToString("F1") + " MB"); // 小数点以下2桁で表示
+
+                    listView1.Items.Add(item);
+                }
+
+            }
+        }
+
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("再生するファイルを選択してください。");
+                return;
+            }
+
+            string selectedFilePath = listView1.SelectedItems[0].Tag.ToString();
+
+            // 同じファイルが選ばれている場合：再生・一時停止の切り替え
+            if (currentFilePath == selectedFilePath)
+            {
+                if (outputDevice != null)
+                {
+                    if (outputDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        outputDevice.Pause();
+                        PlayButton.Text = "再開";
+                        return;
+                    }
+                    else if (outputDevice.PlaybackState == PlaybackState.Paused)
+                    {
+                        outputDevice.Play();
+                        PlayButton.Text = "一時停止";
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                // ファイルが変更された → 前の再生を停止・解放
+                if (outputDevice != null)
+                {
+                    outputDevice.Stop();
+                    outputDevice.Dispose();
+                    outputDevice = null;
+                }
+                if (audioFile != null)
+                {
+                    audioFile.Dispose();
+                    audioFile = null;
+                }
+
+                try
+                {
+                    audioFile = new AudioFileReader(selectedFilePath);
+                    outputDevice = new WaveOutEvent();
+                    outputDevice.Init(audioFile);
+                    outputDevice.Play();
+                    PlayButton.Text = "一時停止";
+                    currentFilePath = selectedFilePath; // 現在のパスを更新
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("再生エラー: " + ex.Message);
+                }
+            }
+        }
+        //Form1のコンストラクタでテキストボックスのドラック＆ドロップを設定
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+         
+
+          
+        }
+        private void TextBox2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect=DragDropEffects.Copy;// ドロップ時にコピー操作
+            }
+            else
+            {
+                e.Effect=DragDropEffects.None;
+            }
+        }
+        private void TextBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            //ドラッグされたファイルのパスを取得
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string filePath in filePaths)
+            {
+                //ファイルが音楽ファイルか確認
+                if(filePath.EndsWith(".mp3",StringComparison.OrdinalIgnoreCase)||filePath.EndsWith(".wav",StringComparison.OrdinalIgnoreCase))
+                {
+                    //ファイルの大きさ取得
+                    FileInfo fi=new FileInfo(filePath);
+                    double fileSizeKB=fi.Length/Byte;
+                    double fileSizeMB = fileSizeKB / Byte;
+                    ListViewItem item = new ListViewItem(fi.Name);
+                    item.SubItems.Add(fi.Extension);
+                    item.SubItems.Add(fi.CreationTime.ToString("yyyy-MM-dd HH:mm"));
+                    item.SubItems.Add(fileSizeMB.ToString("F1") + "MB");
+                    item.Tag = fi.FullName;
+
+                    listView1.Items.Add(item);
+
+                }
+            }
+        }
+        //削除ボタン
+        private void deletion_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("削除する項目を選択してください。");
+                return;
+            }
+            foreach (ListViewItem selectedItem in listView1.SelectedItems)
+            {
+                listView1.Items.Remove(selectedItem);
+            }
+        }
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void form1BindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
